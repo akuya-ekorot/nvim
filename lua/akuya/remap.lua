5ce698bd1332070dd93cfabd49e8cffb99fc9f96
YASHIO_KENYA_11 <akuyaekorot@gmail.com>
2023-07-31 07:56:24 +0300

    split window workflows reset author name to Akuya, from Yashio

--- lua/akuya/remap.lua
+++ lua/akuya/remap.lua
@@ -1,2 +1,9 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", ":Ex<CR>")
+
+vim.keymap.set("n", "<leader>wv", "<C-w>v")
+vim.keymap.set("n", "<leader>ws", "<C-w>s")
+vim.keymap.set("n", "<leader>wl", "<C-w>l")
+vim.keymap.set("n", "<leader>wh", "<C-w>h")
+vim.keymap.set("n", "<leader>wj", "<C-w>j")
+vim.keymap.set("n", "<leader>wk", "<C-w>k")
