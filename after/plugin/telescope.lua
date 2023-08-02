a5fa7a821e82cb97c6120527ffcc5bafcd84298f
YASHIO_KENYA_11 <akuyaekorot@gmail.com>
2023-08-02 12:25:52 +0300

    list buffers

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -2,3 +2,4 @@ local builtin = require('telescope.builtin')
 
 vim.keymap.set('n', '<leader>f', builtin.git_files, {})
 vim.keymap.set('n', '<leader>af', builtin.find_files, {})
+vim.keymap.set('n', '<leader>b', builtin.buffers, {})
