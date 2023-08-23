e83b22f361751322c34ceabc325de21090dc9667
Akuya Ekorot <akuyaekorot@gmail.com>
2023-08-23 23:16:33 +0300

    window resizing and telescope keymaps

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,5 +1,5 @@
 local builtin = require('telescope.builtin')
 
-vim.keymap.set('n', '<leader>f', builtin.git_files, {})
-vim.keymap.set('n', '<leader>af', builtin.find_files, {})
-vim.keymap.set('n', '<leader>b', builtin.buffers, {})
+vim.keymap.set('n', '<leader>ff', builtin.git_files, {})
+vim.keymap.set('n', '<leader>fa', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
